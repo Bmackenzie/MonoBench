3ab319e641917c5dbde38376bc6251794d8cad91
Bryan Mackenzie <confessionsofpain@gmail.com>
2017-06-26 13:59:55 -0700

    init

---
+++ MonoCounterConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System;
+using System.Reflection;
+
+namespace MonoBench
+{
+    public class MonoCounterConfig : MonoBehaviour
+    {
+        static char[] delim = { ',' };
+        public static List<Counter> Counters;
+
+        public struct Counter
+        {
+            public string categoryName;
+            public string counterName;
+            public Counter(string category, string counter)
+            {
+                this.categoryName = category;
+                this.counterName = counter;
+            }
+        }
+
+        public static void GetConfig()
+        {
+            Counters = new List<Counter>();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("MonoConfig.xml");
+            XmlNodeList userNodes = xmlDoc.SelectNodes("//Categories/counter");
+            foreach (XmlNode userNode in userNodes)
+            {
+                string category = userNode.Attributes["name"].Value;
+
+
+                string[] counter = userNode.InnerText.Split(delim);
+                for (int i = 0; i < counter.Length; i++)
+                {
+                    Counters.Add(new Counter(category, counter[i]));
+                }
+            }
+        }
+    }
+}
---
+++ MonoCounterMonitor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+namespace MonoBench
+{
+    public class MonoCounterMonitor : MonoBehaviour
+    {
+        MonoMonitor monitor;
+
+        // Use this for initialization
+        void Start()
+        {
+            MonoBench.MonoCounterConfig.GetConfig();
+            monitor = new MonoMonitor();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                monitor.GetCounterData();
+                UnityEngine.Debug.Log("Getting data");
+            }
+        }
+    }
+
+    class MonoMonitor
+    {
+        private PerformanceCounterCategory[] categories;
+        List<PerformanceCounter> perfCounters;
+
+        public MonoMonitor()
+        {
+            perfCounters = new List<PerformanceCounter>();
+            foreach (MonoBench.MonoCounterConfig.Counter c in MonoBench.MonoCounterConfig.Counters)
+            {
+                perfCounters.Add(new PerformanceCounter(c.categoryName, c.counterName, false));
+            }
+        }
+
+        public void GetCounterData()
+        {
+            Thread thread = new Thread(new ThreadStart(GetData));
+            thread.Start();
+        }
+
+        private void GetData()
+        {
+            try
+            {
+                foreach (PerformanceCounter counter in perfCounters)
+                {
+                    UnityEngine.Debug.Log("" + counter.CounterName + ": " + counter.RawValue.ToString());
+                    UnityEngine.Debug.Log("Value of performance counter " + counter.CategoryName + "/" + counter.CounterName + ": " + counter.RawValue + ", Next Sample: " + counter.NextSample().RawValue + ", Next Value: " + counter.NextValue());
+
+                }
+            }
+            catch (Exception ex)
+            {
+                UnityEngine.Debug.Log("Exception :" + ex.Message);
+                throw;
+            }
+
+            UnityEngine.Debug.Log("Thread going to bed............................");
+            System.Threading.Thread.Sleep(1000);
+            UnityEngine.Debug.Log("Thread going to waking.........................");
+        }
+    }
+}
